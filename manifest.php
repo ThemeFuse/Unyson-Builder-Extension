f31532cf487f56f67acc2d888c8e02dd4293b1d8
moldcraft <moldcraft@email.com>
2014-11-26 19:03:05 +0200

    Added the update parameter

--- manifest.php
+++ manifest.php
@@ -1,3 +1,7 @@
 <?php if (!defined('FW')) die('Forbidden');
 
+$manifest = array();
 
+$manifest['name'] = __( 'Builder', 'fw' );
+$manifest['version'] = '1.0.0';
+$manifest['github_update'] = 'ThemeFuse/Unyson-Builder-Extension';
