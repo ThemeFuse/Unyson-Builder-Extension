9855e25e68041fac8a715cc182cbd28807d22840
moldcraft <moldcraft@email.com>
2014-12-18 16:34:54 +0200

    registered central css grid for frontend

---
+++ static.php
@@ -0,0 +1,10 @@
+<?php if (!defined('FW')) die('Forbidden');
+
+if (!is_admin()) {
+	wp_register_style(
+		'fw-ext-builder-frontend-grid',
+		fw_ext('builder')->get_declared_URI('/static/css/frontend-grid.css'),
+		array(),
+		fw_ext('builder')->manifest->get_version()
+	);
+}
