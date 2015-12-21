1a56724bd402e1d8a426dd00cfcfb056aae5cb5a
moldcraft <moldcraft@email.com>
2015-12-21 13:51:07 +0200

    fixes https://github.com/ThemeFuse/Unyson/issues/1132

--- includes/option-types/builder/static/js/history.js
+++ includes/option-types/builder/static/js/history.js
@@ -1,137 +1,121 @@
 (function ($, fwe, _, localized) {
 
-	fwe.one('fw:option-type:builder:init', function (data) {
+	$(document.body).on('fw:option-type:builder:init', function (e, data) {
+		var elements = {
+				$builder: $(e.target),
+				$undo: $('<a class="disabled undo" href="#">'+ localized.l10n.undo +'</a>'),
+				$redo: $('<a class="disabled redo" href="#">'+ localized.l10n.redo +'</a>')
+			},
+			saveStateFlag = true,
+			builder = data.builder,
+			type = builder.get('type');
 
-		if (!data.$elements.length) {
-			return;
-		}
+		data.$headerTools
+			.removeClass('fw-hidden')
+			.append('<div class="history-container"></div>')
+			.find('> .history-container')
+			.append(elements.$undo)
+			.append(elements.$redo);
 
-		var $builders = data.$elements;
+		var utils = {
+			disableUndo: function () {
+				elements.$undo.addClass('disabled');
+			},
+			enableUndo: function () {
+				elements.$undo.removeClass('disabled');
+			},
+			disableRedo: function () {
+				elements.$redo.addClass('disabled');
+			},
+			enableRedo: function () {
+				elements.$redo.removeClass('disabled');
+			}
+		};
 
-		$builders.each(function () {
+		var history = {
+			storage: [],
+			activeIndex: 0,
+			undo: function () {
+				--this.activeIndex;
 
-			var elements = {
-					$builder: $(this),
-					$undo: $('<a class="disabled undo" href="#">'+ localized.l10n.undo +'</a>'),
-					$redo: $('<a class="disabled redo" href="#">'+ localized.l10n.redo +'</a>')
-				},
-				saveStateFlag = true,
-				type = $(this).attr('data-builder-option-type');
-
-			elements.$builder.find('> .builder-items-types > .fw-builder-header-tools')
-				.removeClass('fw-hidden')
-				.append('<div class="history-container"></div>')
-				.find('> .history-container')
-				.append(elements.$undo)
-				.append(elements.$redo);
-
-			var utils = {
-				disableUndo: function () {
-					elements.$undo.addClass('disabled');
-				},
-				enableUndo: function () {
-					elements.$undo.removeClass('disabled');
-				},
-				disableRedo: function () {
-					elements.$redo.addClass('disabled');
-				},
-				enableRedo: function () {
-					elements.$redo.removeClass('disabled');
+				if ((this.activeIndex === 0)) {
+					utils.disableUndo();
 				}
-			};
 
-			var history = {
-				storage: [],
-				activeIndex: 0,
-				undo: function () {
-					--this.activeIndex;
-
-					if ((this.activeIndex === 0)) {
-						utils.disableUndo();
-					}
-
-					return this.storage[this.activeIndex];
-				},
-				redo: function () {
-					++this.activeIndex;
-					if (this.activeIndex === this.storage.length - 1) {
-						utils.disableRedo();
-					}
-
-					return this.storage[this.activeIndex];
-				},
-				saveState: function (item) {
-					this.storage = _.initial(this.storage, (this.storage.length-1) - this.activeIndex);
-					this.storage.push(item);
-					this.activeIndex = this.storage.length - 1;
+				return this.storage[this.activeIndex];
+			},
+			redo: function () {
+				++this.activeIndex;
+				if (this.activeIndex === this.storage.length - 1) {
+					utils.disableRedo();
 				}
-			};
 
-			fwe.one('fw-builder:' + type + ':register-items', function (builder) {
+				return this.storage[this.activeIndex];
+			},
+			saveState: function (item) {
+				this.storage = _.initial(this.storage, (this.storage.length-1) - this.activeIndex);
+				this.storage.push(item);
+				this.activeIndex = this.storage.length - 1;
+			}
+		};
 
-				//For first time;
-				history.saveState(builder.$input.val());
+		history.saveState(builder.$input.val());
 
-				builder.$input.on('fw-builder:input:change', function () {
+		builder.$input.on('fw-builder:input:change', function () {
+			if (true === saveStateFlag) {
+				history.saveState($(this).val());
+				utils.enableUndo();
+				utils.disableRedo();
+			} else {
+				saveStateFlag = true;
+			}
+		});
 
-					if (true === saveStateFlag) {
-						history.saveState($(this).val());
-						utils.enableUndo();
-						utils.disableRedo();
-					} else {
-						saveStateFlag = true;
-					}
-				});
+		elements.$undo.on('click', function (e) {
+			e.preventDefault();
 
-				elements.$undo.on('click', function (e) {
+			if ($(this).hasClass('disabled')) {
+				return;
+			}
 
-					e.preventDefault();
+			utils.enableUndo();
+			utils.enableRedo();
 
-					if ($(this).hasClass('disabled')) {
-						return;
-					}
-					utils.enableUndo();
-					utils.enableRedo();
+			saveStateFlag = false;
 
-					saveStateFlag = false;
+			var undoSnapshot = history.undo();
 
-					var undoSnapshot = history.undo();
+			if (undoSnapshot !== undefined) {
+				var parsedUndoSnaphot = JSON.parse(undoSnapshot);
+				builder.rootItems.reset(parsedUndoSnaphot);
 
+				if ( parsedUndoSnaphot.length === 0 ) {
+					builder.$input.val('[]');
+				}
+			} else {
+				utils.disableUndo();
+			}
+		});
 
-					if (undoSnapshot !== undefined) {
-						var parsedUndoSnaphot = JSON.parse(undoSnapshot);
-						builder.rootItems.reset(parsedUndoSnaphot);
+		elements.$redo.on('click', function (e) {
+			e.preventDefault();
 
-						if ( parsedUndoSnaphot.length === 0 ) {
-							builder.$input.val('[]');
-						}
+			if ($(this).hasClass('disabled')) {
+				return;
+			}
 
-					} else {
-						utils.disableUndo();
-					}
-				});
+			utils.enableRedo();
+			utils.enableUndo();
 
-				elements.$redo.on('click', function (e) {
-					e.preventDefault();
+			saveStateFlag = false;
 
-					if ($(this).hasClass('disabled')) {
-						return;
-					}
+			var redoSnapshot = history.redo();
 
-					utils.enableRedo();
-					utils.enableUndo();
-
-					saveStateFlag = false;
-
-					var redoSnapshot = history.redo();
-
-					if (redoSnapshot !== undefined) {
-						builder.rootItems.reset(JSON.parse(redoSnapshot));
-					} else {
-						utils.disableRedo();
-					}
-				});
-			});
+			if (redoSnapshot !== undefined) {
+				builder.rootItems.reset(JSON.parse(redoSnapshot));
+			} else {
+				utils.disableRedo();
+			}
 		});
 	});
 })(jQuery, fwEvents, _, _fw_option_type_builder_history);
