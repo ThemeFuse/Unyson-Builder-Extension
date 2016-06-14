d1f02432c9528abcf13b454ae81455510f99345a
moldcraft <moldcraft@email.com>
2016-06-14 15:42:04 +0300

    Version 1.2.3

--- manifest.php
+++ manifest.php
@@ -3,5 +3,5 @@
 $manifest = array();
 
 $manifest['name'] = __( 'Builder', 'fw' );
-$manifest['version'] = '1.2.2';
+$manifest['version'] = '1.2.3';
 $manifest['github_update'] = 'ThemeFuse/Unyson-Builder-Extension';
