911fbb0b622c63d4872944f3970e7f4e558f8edd
moldcraft <moldcraft@email.com>
2016-08-29 21:01:30 +0300

    load option-types only when used

--- class-fw-extension-builder.php
+++ class-fw-extension-builder.php
@@ -7,5 +7,10 @@ class FW_Extension_Builder extends FW_Extension
 	 */
 	protected function _init()
 	{
+		add_action('fw_option_types_init', array($this, '_action_option_types_init'));
+	}
+
+	public function _action_option_types_init() {
+		require dirname( __FILE__ ) . '/includes/option-types/builder/builder.php';
 	}
 }
