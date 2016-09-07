eca41a29d86e6bf336ac1adbef8100cb7eddc8c1
moldcraft <moldcraft@email.com>
2016-09-07 19:16:04 +0300

    silence if user have not updated the framework yet

--- class-fw-extension-builder.php
+++ class-fw-extension-builder.php
@@ -25,7 +25,12 @@ class FW_Extension_Builder extends FW_Extension
 		if ('FW_Option_Type_Builder' === $class) {
 			$this->_action_option_types_init();
 
-			if (is_admin()) {
+			if (
+				is_admin()
+				&&
+				// https://github.com/ThemeFuse/Unyson-Extensions-Approval/issues/258
+				version_compare(fw()->manifest->get_version(), '2.6.2', '>')
+			) {
 				FW_Flash_Messages::add(
 					'builder-option-type-register-wrong',
 					__("Please register builder types on 'fw_option_types_init' action", 'fw'),
@@ -35,7 +40,12 @@ class FW_Extension_Builder extends FW_Extension
 		} elseif ('FW_Option_Type_Builder_Item' === $class) {
 			require dirname( __FILE__ ) . '/includes/option-types/builder/extends/class-fw-option-type-builder-item.php';
 
-			if (is_admin()) {
+			if (
+				is_admin()
+				&&
+				// https://github.com/ThemeFuse/Unyson-Extensions-Approval/issues/258
+				version_compare(fw()->manifest->get_version(), '2.6.2', '>')
+			) {
 				FW_Flash_Messages::add(
 					'builder-item-type-register-wrong',
 					__("Please register builder item-types on 'fw_option_type_builder:{builder-type}:register_items' action", 'fw'),
