e838c243ea36f2c86c5c2fb695e9d27eb3dcfb23
Andrei Glingeanu <andrei.glingeanu@gmail.com>
2016-11-10 19:02:15 +0200

    Update manifest.php

--- manifest.php
+++ manifest.php
@@ -6,6 +6,6 @@ $manifest['name'] = __( 'Builder', 'fw' );
 $manifest['version'] = '1.2.5';
 $manifest['github_update'] = 'ThemeFuse/Unyson-Builder-Extension';
 $manifest['github_repo'] = 'https://github.com/ThemeFuse/Unyson-Builder-Extension';
-$manifest['uri'] = 'http://manual.unyson.io/en/latest/extension/builder/index.html;';
+$manifest['uri'] = 'http://manual.unyson.io/en/latest/extension/builder/index.html';
 $manifest['author'] = 'ThemeFuse';
 $manifest['author_uri'] = 'http://themefuse.com/';
