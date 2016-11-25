95ba1d95bc2d20ad70e946e53aac20d9422eabe1
moldcraft <moldcraft@email.com>
2016-11-25 11:50:03 +0200

    indentation fixes

--- includes/option-types/builder/includes/templates/static/scripts.js
+++ includes/option-types/builder/includes/templates/static/scripts.js
@@ -8,8 +8,8 @@
 				tooltipContent: $('<div class="fw-builder-templates-tooltip-content"></div>'),
 				tooltipLoading: $(
 					'<div class="fw-builder-templates-tooltip-loading">'+
-						'<div class="loading-icon fw-animation-rotate-reverse-180 unycon unycon-unyson-o"></div>'+
-						'</div>'
+					/**/'<div class="loading-icon fw-animation-rotate-reverse-180 unycon unycon-unyson-o"></div>'+
+					'</div>'
 				),
 				headerTools: data.$headerTools
 			},
@@ -53,10 +53,10 @@
 
 					this.$el.tooltipContent.html(json.data.html);
 
-						/**
-					* Html was replaced
-					* Components that have html in tooltip, must init js events
-					*/
+					/**
+					 * Html was replaced
+					 * Components that have html in tooltip, must init js events
+					 */
 					fwe.trigger('fw:option-type:builder:templates:init', {
 						$elements: this.$el.tooltipContent,
 						builder: this.builder,
@@ -73,9 +73,8 @@
 
 					fw.soleModal.show(
 						'fw-builder-templates-error',
-						'<h4>Ajax Error</h4>'+
-							'<p class="fw-text-danger">'+ error +'</p>',
-							{showCloseButton:false}
+						'<h4>Ajax Error</h4><p class="fw-text-danger">'+ error +'</p>',
+						{showCloseButton:false}
 					);
 					console.error('Ajax error', error);
 				}, this));
@@ -86,106 +85,101 @@
 			.removeClass('fw-hidden')
 			.append(
 				'<div class="template-container fw-pull-right">' +
-					'<a class="template-btn" href="#" onclick="return false;">'+ localized.l10n.templates +'</a>' +
-					'</div>'
+				/**/'<a class="template-btn" href="#" onclick="return false;">'+ localized.l10n.templates +'</a>' +
+				'</div>'
 			);
 
-			inst.tooltipApi = inst.$el.headerTools
-				.find('.template-container .template-btn')
-				.qtip({
-					show: 'click',
-					hide: 'unfocus',
-					position: {
-						at: 'bottom center',
-						my: 'top center',
-						viewport: $(document.body)
-					},
-					events: {
-						show: function () {
-							inst.refresh();
-						}
-					},
-					style: {
-						classes: 'qtip-fw qtip-fw-builder',
-						tip: {
-							width: 12,
-							height: 5
-						},
-						width: 180
-					},
-					content: {
-						text: inst.$el.tooltipContent
+		inst.tooltipApi = inst.$el.headerTools
+			.find('.template-container .template-btn')
+			.qtip({
+				show: 'click',
+				hide: 'unfocus',
+				position: {
+					at: 'bottom center',
+					my: 'top center',
+					viewport: $(document.body)
+				},
+				events: {
+					show: function () {
+						inst.refresh();
 					}
-				})
-				.qtip('api');
+				},
+				style: {
+					classes: 'qtip-fw qtip-fw-builder',
+					tip: {
+						width: 12,
+						height: 5
+					},
+					width: 180
+				},
+				content: {
+					text: inst.$el.tooltipContent
+				}
+			})
+			.qtip('api');
 
-				/**
-			* Accordion
-			*/
-				inst.$el.tooltipContent
-					.on(
-						'click',
-						'.fw-builder-templates-types > .fw-builder-templates-type > .fw-builder-templates-type-title',
-						function() {
-							var $wrapper = $(this).closest('.fw-builder-templates-type'),
-								$content = $wrapper.find('> .fw-builder-templates-type-content'),
-								$root = $wrapper.closest('.fw-builder-templates-types'),
-								specialClass = 'current';
+			/**
+			 * Accordion
+			 */
+			inst.$el.tooltipContent
+				.on(
+					'click',
+					'.fw-builder-templates-types > .fw-builder-templates-type > .fw-builder-templates-type-title',
+					function() {
+						var $wrapper = $(this).closest('.fw-builder-templates-type'),
+							$content = $wrapper.find('> .fw-builder-templates-type-content'),
+							$root = $wrapper.closest('.fw-builder-templates-types'),
+							specialClass = 'current';
 
-							if ($root.hasClass('is-busy')) {
-								return;
-							} else {
-								$root.addClass('is-busy');
-							}
+						if ($root.hasClass('is-busy')) {
+							return;
+						} else {
+							$root.addClass('is-busy');
+						}
 
-							$content.addClass(specialClass);
+						$content.addClass(specialClass);
 
-							$root
-								.find('> .fw-builder-templates-type > .fw-builder-templates-type-content:not(.'+specialClass+'):not(.fw-hidden)')
-								.slideUp(function(){
-									$(this).addClass('fw-hidden').removeAttr('style');
+						$root
+							.find('> .fw-builder-templates-type > .fw-builder-templates-type-content:not(.'+specialClass+'):not(.fw-hidden)')
+							.slideUp(function(){
+								$(this).addClass('fw-hidden').removeAttr('style');
+							});
+
+						$content.removeClass(specialClass);
+						inst.$el.tooltipContent.removeAttr('data-open-type');
+
+						if ($content.hasClass('fw-hidden')) {
+							$content
+								.css('display', 'none')
+								.removeClass('fw-hidden')
+								.slideDown(function(){
+									$root.removeClass('is-busy');
+									$(this).removeAttr('style');
+									inst.$el.tooltipContent.attr('data-open-type', $wrapper.attr('data-type'));
 								});
-
-								$content.removeClass(specialClass);
-
-								inst.$el.tooltipContent.removeAttr('data-open-type');
-
-								if ($content.hasClass('fw-hidden')) {
-									$content
-										.css('display', 'none')
-										.removeClass('fw-hidden')
-										.slideDown(function(){
-											$root.removeClass('is-busy');
-
-											$(this).removeAttr('style');
-
-											inst.$el.tooltipContent.attr('data-open-type', $wrapper.attr('data-type'));
-										});
-								} else {
-									$content
-										.slideUp(function(){
-											$root.removeClass('is-busy');
-
-											$(this).addClass('fw-hidden').removeAttr('style');
-										});
-								}
+						} else {
+							$content.slideUp(function(){
+								$root.removeClass('is-busy');
+								$(this).addClass('fw-hidden').removeAttr('style');
+							});
 						}
-					)
-					.on('fw:option-type:builder:templates:after-html-replace', function(){
-						// reopen accordion type that was open before tooltip html replace
-						{
-							var openType = inst.$el.tooltipContent.attr('data-open-type');
+					}
+				)
+				.on('fw:option-type:builder:templates:after-html-replace', function(){
+					// reopen accordion type that was open before tooltip html replace
+					{
+						var openType = inst.$el.tooltipContent.attr('data-open-type');
 
-							if (openType) {
-								inst.$el.tooltipContent // close all
-									.find('.fw-builder-templates-types > .fw-builder-templates-type > .fw-builder-templates-type-content')
-									.addClass('fw-hidden');
+						if (openType) {
+							inst.$el.tooltipContent // close all
+								.find('.fw-builder-templates-types > .fw-builder-templates-type > .fw-builder-templates-type-content')
+								.addClass('fw-hidden');
 
-								inst.$el.tooltipContent // open one
-									.find('.fw-builder-templates-types > .fw-builder-templates-type-'+ openType +' > .fw-builder-templates-type-content')
-									.removeClass('fw-hidden');
-							}
+							inst.$el.tooltipContent // open one
+								.find('.fw-builder-templates-types > .fw-builder-templates-type-'+ openType +' > .fw-builder-templates-type-content')
+								.removeClass('fw-hidden');
 						}
-					});
+					}
+				});
 	});
 })(jQuery, fwEvents, _, _fw_option_type_builder_templates);
