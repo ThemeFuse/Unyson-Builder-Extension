cf60ed76472018186f2bedfc0c5bd0e6fd6993a7
Cristeam <dev@cristeam.com>
2016-12-02 15:18:46 +0200

    Applied autoload

--- includes/option-types/builder/includes/templates/components/full/init.php
+++ includes/option-types/builder/includes/templates/components/full/init.php
@@ -2,8 +2,6 @@
 
 add_action('fw_ext_builder:template_components_register', '_action_fw_ext_builder_template_component_full', 3);
 function _action_fw_ext_builder_template_component_full() {
-	require_once dirname(__FILE__) .'/class-fw-ext-builder-templates-component-full.php';
-
 	FW_Ext_Builder_Templates::register_component(
 		new FW_Ext_Builder_Templates_Component_Full()
 	);
--- includes/option-types/builder/includes/templates/init.php
+++ includes/option-types/builder/includes/templates/init.php
@@ -1,8 +1,5 @@
 <?php if (!defined('FW')) die('Forbidden');
 
-require_once dirname(__FILE__) .'/class-fw-ext-builder-templates-component.php';
-require_once dirname(__FILE__) .'/class-fw-ext-builder-templates.php';
-
 foreach (array('full') as $component_id) {
 	require_once dirname(__FILE__) .'/components/'. $component_id .'/init.php';
 }
